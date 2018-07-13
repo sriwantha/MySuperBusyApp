ff2ae518be5dcd2c20fb69ebca19ff302f17f23e
sriwanth <sriwanth@SYD12-O9K98K3>
2018-07-13 10:19:08 +1000

    Add project files.

---
+++ MySuperBusyApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+namespace MyBatchApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Task> jobs = new List<Task>();
+            //start 20 background tasks that will keep the CPU bussy
+            for (int i = 0; i < 20; i++)
+            {
+                string id = "super busy worker " + i;
+                jobs.Add(Task.Factory.StartNew(new Action<object>(Run), id));
+            }
+            //do not exit, continue forever
+            Task.WaitAll(jobs.ToArray());
+        }
+
+        public static void Run(Object param)
+        {
+            while (true)
+            {
+                //sleep for 500ms so we won't burn all CPU
+                string stamp = DateTime.Now.ToString("HH:mm:ss") + "-->" 
+                    + Math.Sqrt(DateTime.Now.Millisecond);
+
+                Console.WriteLine($"{param}:{stamp}");
+            }
+        }
+    }
+}
